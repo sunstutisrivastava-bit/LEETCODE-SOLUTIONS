bddb5bd861a255ce5f8a2ca498f0fc2304b189b8
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-08-16 22:50:22 +0530

    Time: 8 ms (30.2%), Space: 131.2 MB (92.89%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
         int cnt[3] = {0};
-
         for (int x : stones) {
             cnt[x % 3]++;
         }
-
         int c0 = cnt[0];
         int c1 = cnt[1];
         int c2 = cnt[2];
-
-        // No stones with remainder 1 or 2
         if (c1 == 0 && c2 == 0)
             return false;
-
-        // If number of 0-reminder stones is even
         if (c0 % 2 == 0) {
             return c1 > 0 && c2 > 0;
         }
-
-        // If number of 0-reminder stones is odd
         return abs(c1 - c2) > 2;
     }
 };
