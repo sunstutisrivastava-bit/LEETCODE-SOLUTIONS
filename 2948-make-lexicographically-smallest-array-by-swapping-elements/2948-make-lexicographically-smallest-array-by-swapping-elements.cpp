9a89169e17e24cdc20bba08000e489416e2eee35
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-08-29 12:23:04 +0530

    Time: 132 ms (87.32%), Space: 149.1 MB (83.94%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -3,8 +3,6 @@ public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         
         int n = nums.size();
-
-        // value, original index
         vector<pair<int,int>> v;
 
         for (int i = 0; i < n; i++) {
@@ -18,21 +16,16 @@ public:
         while (i < n) {
 
             int j = i;
-
-            // Find all values that can be connected
             while (j + 1 < n &&
                    v[j + 1].first - v[j].first <= limit) {
                 j++;
             }
-
-            // Original indices of this group
             vector<int> ind;
 
             for (int k = i; k <= j; k++) {
                 ind.push_back(v[k].second);
             }
 
-            // Smallest index gets smallest value
             sort(ind.begin(), ind.end());
 
             for (int k = 0; k < ind.size(); k++) {
