71ddc6313805e6508b692b0e6c39b4621e355519
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-09-02 11:29:22 +0530

    Time: 0 ms (100%), Space: 30.2 MB (94.95%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int i,j,l,a,b;
+        vector<int>nums2(l);
+        vector<int>nums3(l);
+        return true;
+    }
+};
