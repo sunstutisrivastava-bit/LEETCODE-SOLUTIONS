fd6df4e068f9f95945a4f630df2404fb4ee7b9c3
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-09-09 14:45:06 +0530

    Time: 0 ms (100%), Space: 27.2 MB (80.92%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int i,j,l;
+       i=0;
+       j=s.size()-1;
+       while(i<j){
+        char ch = s[i];
+        s[i]=s[j];
+        s[j]=ch;
+        i++;
+        j--;
+       } 
+    }
+};
