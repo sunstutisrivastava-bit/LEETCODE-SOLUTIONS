fc0d346c4de0adacecf675c261d951432e5b1103
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-09-09 14:41:23 +0530

    Time: 335 ms (5.33%), Space: 9.9 MB (52.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int i,j,c=0,k=0;
+       i=0;
+       j=s.size()-1;
+       if(j+1==1){
+        return true;
+       }
+       while(i<j){
+        if(s[i]>=65&&s[i]<=90) s[i]=tolower(s[i]);
+        else if(!((s[i]>=97&&s[i]<=122)||(s[i]>=48&&s[i]<=57))){
+            i++;
+            cout<<i<<"i"<<endl;
+            continue;
+        }
+        if(s[j]>=65&&s[j]<=90) s[j]=tolower(s[j]);
+        else if(!((s[j]>=97&&s[j]<=122)||(s[j]>=48&&s[j]<=57))) {
+            j--;
+            cout<<j<<"j"<<endl;
+            continue;
+        }
+        cout<<s[i]<<"  "<<s[j]<<endl;
+        if(s[i]==s[j]){
+            c++;
+            
+        } 
+        k++; 
+        i++;
+        j--;     
+
+       }
+       cout<<k;
+       if(c==k){
+        return true;
+       }
+       else{
+        return false;
+       }
+    }
+};
