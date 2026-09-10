0bc905ed6bacec4cfecbcf0d93c0f2490257f331
sunstutisrivastava-bit <sunstutisrivastava@gmail.com>
2026-09-11 00:15:03 +0530

    Time: 3 ms (91.06%), Space: 15.8 MB (64.72%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+
+    pair<int, int> solve(TreeNode* root) {
+        if (root == NULL)
+            return {0, 0};
+
+        pair<int, int> left = solve(root->left);
+        pair<int, int> right = solve(root->right);
+
+        int sum = root->val + left.first + right.first;
+        int count = 1 + left.second + right.second;
+
+        if (root->val == sum / count)
+            ans++;
+
+        return {sum, count};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};
